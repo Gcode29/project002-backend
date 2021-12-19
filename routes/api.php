94044e1f9164cd9870ec002f81f208496900c231
Gcode29 <agentgeorgeuy29@gmail.com>
2021-12-19 21:53:44 +0800

    test

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,36 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\PageController;
+
+Route::post('login', [AuthController::class, 'login']);
+
+// Route::post('/login', function (Request $request) {
+//     $data = $request->validate([
+//         'email' => 'required|email',
+//         'password' => 'required'
+//     ]);
+
+//     $user = User::where('email', $request->email)->first();
+
+//     if (!$user || !Hash::check($request->password, $user->password)) {
+//         return response([
+//             'message' => ['These credentials do not match our records.']
+//         ], 404);
+//     }
+
+//     $token = $user->createToken('my-app-token')->plainTextToken;
+
+//     $response = [
+//         'user' => $user,
+//         'token' => $token
+//     ];
+
+//     return response($response, 201);
+// });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
