8276df8927d8a4b256c5bf5acb7843722964faa2
Mark <mrkalmdn@gmail.com>
2022-01-05 22:11:46 +0800

    Add categories

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 
 Route::post('login', [AuthController::class, 'store'])->name('login');
 
@@ -11,4 +12,6 @@ Route::post('login', [AuthController::class, 'store'])->name('login');
 Route::group(['middleware' => 'auth:sanctum'], function() {
     Route::get('me', [AuthController::class, 'me'])->name('me');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::apiResource('categories', CategoryController::class);
 });
